5c49ef0a9e4ddf6d7d4c92b4e47963d1481dd8eb
sundarganeshk <sundarganeshk@rediffmail.com>
2017-03-20 11:24:49 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+console.log('Loaded!');
+
+var submit = document.getElementById('submit_btn');
+submit.onclick = function () {
+
+    // create a request object
+var request = new XMLHttpRequest();
+
+// capture the response and store it in a variable
+request.onreadyStatechange = function () {
+    if (request.readyState === XMLHttpRequest.DONE) {
+    // take some action
+    if (request.status === 200) {
+      alert("logged in successfully");
+    }else if (request.status === 403) {
+        alert('username/password is incorrect');
+    }else if (request.status === 500) {
+        alert ('Something Went wrong on the server');
+    }
+    }
+
+// not done yet
+
+};
+// make the request
+var username = document.getElementById('username').value;
+var password = document.getElementById('password').value;
+console.log(username);
+console.log(password);
+request.open('POST','http://localhost:8080/login', true);
+request.setRequestHeader('Content-Type', 'application/json');
+request.send(JSON.stringify({username: username, password: password}));
+
+};
